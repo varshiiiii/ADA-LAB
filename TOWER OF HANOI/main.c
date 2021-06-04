a3763dc660d96162be6488907bef83c637edc0d1
varshiiiii <71704727+varshiiiii@users.noreply.github.com>
2021-06-04 17:11:32 +0530

    Create main.c

---
+++ TOWER OF HANOI/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void towerOfHanoi(int n,char from_rod,char to_rod,char aux_rod){
+    if(n==1){
+        printf("move disk from %c to %c\n",from_rod,to_rod);
+        return;
+    }
+    towerOfHanoi(n-1 , from_rod, aux_rod, to_rod);
+    printf("move disk from %c to %c\n",from_rod,to_rod);
+    towerOfHanoi(n-1, aux_rod, to_rod, from_rod);
+}
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int n=3;
+    towerOfHanoi(n,'a','c','b');
+    return 0;
+}
